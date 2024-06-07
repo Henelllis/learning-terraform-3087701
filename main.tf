fc8129652e96fa4b6917dc0074ff1f2afa2929d9
Henelllis <hellis1001@email.com>
2024-06-07 14:07:47 -0400

    create new dev VPC

--- main.tf
+++ main.tf
@@ -18,6 +18,21 @@ data "aws_vpc" "default" {
   default = true
 }
 
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
@@ -26,6 +41,7 @@ resource "aws_instance" "blog" {
     Name = "HelloWorld"
   }
 
+  subnet_id = module.blog_vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 }
 
@@ -33,7 +49,7 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.2"
   name = "blog_new"
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.blog_vpc.vpc_id
 
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
